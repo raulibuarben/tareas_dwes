04fa2b87136445712ee38af7db2798481d31cb14
raulibuarben <raulibu@gmail.com>
2025-12-09 18:19:14 +0100

    Vista funcionando

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Tarea
 
 # Create your views here.
+class DetalleTareaView(DetailView):
+    model = Tarea
+    template_name = 'tareas/detalle_tarea.html'
+    context_object_name = 'tarea'
